294b72b8a20223d34ba4629ccb19a7d14b150520
Gilberto1397 <gilberto-junior@outlook.com>
2022-03-28 16:04:21 -0300

    Configurações de ambiente

--- repository/Conexao.php
+++ repository/Conexao.php
@@ -6,6 +6,20 @@ class Conexao
 {
     public static function criar(): PDO    // static permitindo chamar o método sem necessitade de instanciar 
     {
-        return new PDO("sqlite:db/filmes.db");
+        $env = parse_ini_file('.env'); // função para puxar as variáveis de ambiente
+        
+        $databaseType = $env["databasetype"];
+        $database = $env["database"];
+        $server = $env["server"];
+        $pass = $env["pass"];
+        $user = $env["user"];
+
+        if ($databaseType === "mysql") {    // controle para trocar para mysql no ambiente de produção
+            $database = "host=$server;dbname=$database";
+        }
+
+        return new PDO("$databaseType:$database");
+
+        //return new PDO("sqlite:db/filmes.db");
     }
 }
