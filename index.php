24575b4faeca316f4c209cf6b13e98b460283b46
Gilberto1397 <gilberto-junior@outlook.com>
2022-04-19 20:21:28 -0300

    Update index.php

--- index.php
+++ index.php
@@ -34,23 +34,3 @@ if (substr($rota, 0, strlen("/filmes")) === "/filmes") {
 }
 
 require "views/404.php";
-
-
-/* switch ($rota) {
-    case "/":
-        require "views/galeria.php";
-        break;
-
-    case "/novo":   //  tratamento para apenas acessar a página de cadastro e então quando estiver na mesma página se acontecer o POST irá redirecionar e executar a rotina
-        if($metodo == "GET") require "views/cadastrar.php";
-        if($metodo == "POST") 
-        {
-            $controller = new FilmesControoler();
-            $controller->save($_REQUEST); // $_REQUEST - armazena os dados enviados do form
-        }
-        break;
-    
-    default:
-        require "views/404.php";
-        break;
-} */
